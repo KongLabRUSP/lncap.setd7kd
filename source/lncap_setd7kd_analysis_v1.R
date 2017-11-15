899f5a2814f28faac936755f5fc25cca784100e5
Sargsyan <dsargsy@its.jnj.com>
2017-11-14 22:12:59 -0500

    MOved data files outside the project folder to speed up RStudio

--- source/lncap_setd7kd_analysis_v1.R
+++ source/lncap_setd7kd_analysis_v1.R
@@ -12,7 +12,7 @@
 require(data.table)
 require(ggplot2)
 
-# PArt I: Data----
+# Part I: Data----
 lData <- dir("data")
 lData
 
@@ -20,72 +20,105 @@ dt1 <- fread(paste("data",
                    lData[1],
                    sep = "/"),
              skip = 4,
-             header = TRUE)[, c(1, 3, 5, 6, 9)]
-dt1$`Other ID` <- as.numeric(dt1$`Other ID`)
-dt1 <- subset(dt1,
-              !is.na(`Other ID`))
+             header = TRUE)[, c(1, 3, 5, 6, 13)]
 names(dt1)[1:2] <- c("LNCaP",
                      "LNCaP PEITC")
+dt1 <- subset(dt1,
+              `Gene ID` != "" &
+                `Gene ID` != "-")
+
+dt1$`Gene ID`
 dt1
 
 dt4 <- fread(paste("data",
                    lData[4],
                    sep = "/"),
              skip = 5,
-             header = TRUE)[, c(1, 3, 5, 6, 9)]
-dt4$`Other ID` <- as.numeric(dt4$`Other ID`)
-dt4 <- subset(dt4,
-              !is.na(`Other ID`))
+             header = TRUE)[, c(1, 3, 5, 6, 9, 13)]
 names(dt4)[1:2] <- c("Setd7",
                      "Setd7 PEITC")
+dt4 <- subset(dt4,
+              `Gene ID` != "" &
+                `Gene ID` != "-")
+
+dt4$`Gene ID`
 dt4
 
+# dtt <- merge(dt1,
+#              dt4,
+#              by = "Gene ID",
+#              all = TRUE)
 dtt <- merge(dt1,
              dt4,
-             by = "Other ID")
-names(dtt) [1] <- "GeneID"
+             by = "Gene ID")
 dtt
 
 # Heatmap: all 4 groups----
 dtl <- melt.data.table(dtt,
-                       id.vars = "GeneID",
+                       id.vars = "Gene ID",
                        measure.vars = c(2, 3, 6, 7),
                        variable.name = "Group",
                        value.name = "Readout")
-dtl$GeneID <- factor(dtl$GeneID)
+dtl$`Gene ID` <- factor(dtl$`Gene ID`)
 dtl$Group <- factor(dtl$Group,
-                    levels = unique(dtl$Group))
+                    levels = c("LNCaP",
+                               "Setd7",
+                               "LNCaP PEITC",
+                               "Setd7 PEITC"))
 dtl$Readout <- as.numeric(dtl$Readout)
-range(dtl$Readout)
+range(dtl$Readout, na.rm = TRUE)
 dtl
+
+# Plot all annotated genes found in all 4 samples----
+p0 <- ggplot(data = dtl) +
+  geom_tile(aes(x =  Group,
+                y = `Gene ID`,
+                fill = Readout),
+            color = "black") +
+  scale_fill_gradient2(high = "red",
+                       limit = range(dtl$Readout),
+                       name = "Readout") +
+  scale_x_discrete(expand = c(0, 0)) +
+  scale_y_discrete("Gene ID",
+                   expand = c(0, 0)) +
+  ggtitle("All Annotated Genes Found Across All 4 Samples") +
+  theme(axis.text.x = element_text(angle = 45,
+                                   hjust = 1),
+        # legend.position = "top",
+        plot.title = element_text(hjust = 0.5))
+p0
+
+tiff(filename = "tmp/all_anno_genes.tiff",
+     height = 7,
+     width = 6,
+     units = 'in',
+     res = 300,
+     compression = "lzw+p")
+print(p0)
+graphics.off()
 
 # Genes orered by ratios---
 dtt$Ratio.x <- as.numeric(dtt$Ratio.x)
-range(dtt$Ratio.x)
+range(dtt$Ratio.x, na.rm = TRUE)
 
 dtt$Ratio.y <- as.numeric(dtt$Ratio.y)
-range(dtt$Ratio.y)
-
-top20lc <- dtt$GeneID[order(dtt$Ratio.x,
-                            decreasing = TRUE)][1:20]
-# bot20lc <- dtt$GeneID[order(dtt$Ratio.x,
-#                             decreasing = FALSE)][1:20]
-top20s7 <- dtt$GeneID[order(dtt$Ratio.y,
-                            decreasing = TRUE)][1:20]
-# bot20s7<- dtt$GeneID[order(dtt$Ratio.y,
-#                            decreasing = FALSE)][1:20]
+range(dtt$Ratio.y, na.rm = TRUE)
 
 # # Select 20 genes at random to plot
 # keepID <- sample(levels(dtl$GeneID), 20)
 # tmp <- droplevels(dtl[GeneID %in% keepID, ])
 
 # Top LNCaP genes (by ratio)----
-tmp <- droplevels(dtl[GeneID %in% top20lc, ])
+tmp <- subset(dtt,
+              !is.na(dtt$Ratio.x))
+top20lc <- tmp$`Gene ID`[order(tmp$Ratio.x,
+                               decreasing = TRUE)][1:20]
+tmp <- droplevels(dtl[`Gene ID` %in% top20lc, ])
 # tmp <- droplevels(dtl[GeneID %in% bot20lc, ])
 
 p1 <- ggplot(data = tmp) +
   geom_tile(aes(x =  Group,
-                y = GeneID,
+                y = `Gene ID`,
                 fill = Readout),
             color = "black") +
   scale_fill_gradient2(high = "red",
@@ -111,6 +144,10 @@ print(p1)
 graphics.off()
 
 # Top Setd7 genes (by ratio)----
+tmp <- subset(dtt,
+              !is.na(dtt$Ratio.y))
+top20s7 <- tmp$`Gene ID`[order(tmp$Ratio.y,
+                               decreasing = TRUE)][1:20]
 tmp <- droplevels(dtl[GeneID %in% top20s7, ])
 # tmp <- droplevels(dtl[GeneID %in% bot20lc, ])
 
@@ -140,3 +177,37 @@ tiff(filename = "tmp/Setd7_Setd7-PEITC.tiff",
      compression = "lzw+p")
 print(p2)
 graphics.off()
+
+# Entrez ID to gene name
+entraz_id <- list()
+for (i in 1:length(gene_names)) {
+  out <- try(unlist(mget(x = gene_names[i],
+                         envir = org.Mm.egALIAS2EG)),
+             silent = TRUE)
+  if (class(out)[1] != "try-error") {
+    entraz_id[i] <- out
+  } else {
+    entraz_id[i] <- NA
+  }
+}
+
+entraz_id <- list()
+
+gene_names <- c("BC137383")
+for (i in 1:length(gene_names)) {
+  out <- try(unlist(mget(x = gene_names[i],
+                         envir = org.Mm.egACCNUM2EG)),
+             silent = TRUE)
+  if (class(out)[1] != "try-error") {
+    entraz_id[i] <- out
+  } else {
+    entraz_id[i] <- NA
+  }
+}
+
+entraz_id
+
+org.Mm.eg.db
+org.Mm.eg_dbInfo()
+org.Mm.egACCNUM
+org.Mm.egACCNUM2EG@
